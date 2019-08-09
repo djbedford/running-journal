b688720eee192639adfa0c7922fd69b490119c5f
djbedford <beds2004@hotmail.com>
2019-08-09 07:09:53 +0100

    Include link to gridsome.org

--- src/components/JournalFooter.vue
+++ src/components/JournalFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="layout footer">
-        <p>Copyright &copy; 2019. Powered by Gridsome</p>
+        <p>Copyright &copy; 2019. Powered by <a href="//gridsome.org">Gridsome</a></p>
     </footer>
 </template>
 
@@ -9,5 +9,14 @@
     text-align: center;
     color: #3D3D3D;
     font-size: .8rem;
+
+    a {
+        text-decoration: none;
+        border-bottom: #3D3D3D 1px solid;
+
+        &:visited {
+            color: #3D3D3D;
+        }
+    }
 }
 </style>
